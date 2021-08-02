ad2e2318cd6232d99729c4cf6185c3d6962b8a2e
yh1120 <yonghun1120@gmail.com>
2021-08-02 23:19:46 +0900

    Feat: #6 getUserInfo and getUserMenubar

---
+++ src/utils/constants.js
@@ -0,0 +1,11 @@
+import { getUserInfo } from './getUserInfo';
+
+export const USER_INFO = getUserInfo(); // 암호화 된 정보 제외한 정보들만 출력
+// {
+//     id: user.id,
+//       name: user.name,
+//       age: user.age,
+//       role: user.role,
+//       address: user.address,
+//       menubar: user.menubar,
+// }
