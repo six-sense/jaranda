b4fe33cbbe6def326d18ef5140f645a4ffea989e
hurima90-kim <hurima90@gmail.com>
2021-08-02 20:30:18 +0900

    Chore: Add styled-components package, setting global styles and setting absolute path

---
+++ src/Styles/GlobalStyles.js
@@ -0,0 +1,23 @@
+import { createGlobalStyle } from 'styled-components';
+import reset from 'styled-reset'; // style-reset 패키지
+
+const GlobalStyles = createGlobalStyle` 
+    ${reset}
+    a{
+        text-decoration: none;
+        color: inherit;
+    }
+    *{
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+    }
+    body {
+        font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+        font-size: 14px;
+        background-color: #fff;
+        color: #000;
+    }
+`;
+
+export default GlobalStyles;
