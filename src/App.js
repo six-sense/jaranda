b7d214e9474112527fcca99104fc336dd6913ef6
UlongChaS2 <jyujung2@gmail.com>
2021-08-02 22:40:34 +0900

    Feat: #7 done Login layout

--- src/App.js
+++ src/App.js
@@ -1,8 +1,5 @@
-<<<<<<< HEAD
+import React from 'react';
 import Login from 'Pages/Login';
-=======
-import Login from './Pages/Login/Login.js';
->>>>>>> 76c039c (Feat: Login layout)
 
 function App() {
   return <Login />;
