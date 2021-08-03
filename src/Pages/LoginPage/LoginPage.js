ac53ed5d7bf3424f90255e0e8984dc113dcfa413
Telling-Y <mint1480123@gmail.com>
2021-08-03 23:42:22 +0900

    feat/add login validation routing

--- src/Pages/LoginPage/LoginPage.js
+++ src/Pages/LoginPage/LoginPage.js
@@ -1,52 +1,60 @@
-import React, { useState } from 'react';
+import React, { useState } from 'react'; // , { useState }
+import { useHistory } from 'react-router-dom';
 import { Validation } from 'utils/checkValid';
+import { LOCAL_STORAGE } from 'utils/constants';
 import { style } from './LoginPageStyle';
 
 export default function Login() {
-  const { checkId, checkPassword } = Validation;
+  const [isVaild, setIsVaild] = useState(false);
   const [inputIdValue, setInputIdValue] = useState('');
   const [inputPwValue, setInputPwValue] = useState('');
-  const [isVaild, setIsVaild] = useState(false);
+  const history = useHistory();
+  const { checkId, checkPassword } = Validation;
 
   const handleIdInput = (e) => {
-    const { value } = e.target;
-    if (checkId(value)) {
-      setInputIdValue(value);
+    setInputIdValue(e.target.value);
+  };
+
+  const handlePwInput = (e) => {
+    setInputPwValue(e.target.value);
+  };
+
+  const sendLogin = async (userID, userPW) => {
+    const userInfo = await LOCAL_STORAGE.get('userData');
+    const test =
+      userInfo &&
+      userInfo?.find(
+        (data) => data.userId === userID && data.password === userPW,
+      );
+    if (test !== undefined) {
+      await LOCAL_STORAGE.set('token', {
+        userId: test.userId,
+        role: test.role,
+      });
+      return true;
+    }
+    return false;
+  };
+
+  const checkLogin = () => {
+    const validLogin = sendLogin(inputIdValue, inputPwValue);
+    if (
+      (checkId(inputIdValue) === true, checkPassword(inputPwValue) === true)
+    ) {
+      if (validLogin && LOCAL_STORAGE.get('token').role === 'admin') {
+        history.push('/admin');
+      } else {
+        history.push('/');
+      }
+    } else {
+      setIsVaild(true);
+      setTimeout(() => {
+        setIsVaild(false);
+      }, 6000);
     }
   };
 
   console.log(inputIdValue);
-
-  const handlePwInput = (e) => {
-    const { value } = e.target;
-    if (checkPassword(value)) {
-      setInputPwValue(value);
-    }
-  };
-
-  const removeInputText = () => {
-    setInputIdValue('');
-    setInputPwValue('');
-  };
-
-  const checkLogin = () => {
-    if (isVaild === true) {
-      // if (
-      //   localStorage.getItem(inputIdValue) &&
-      //   localStorage.getItem(inputPwValue)
-      // )
-      localStorage.setItem('token', { userId: 'jung', role: 'teacher' });
-      removeInputText();
-    } else {
-      setIsVaild(true);
-      setTimeout(() => {
-        setIsVaild(true);
-      }, 6000);
-
-      removeInputText();
-    }
-  };
-
   return (
     <Container>
       <Wrap>
@@ -56,11 +64,9 @@ export default function Login() {
             유효한 아이디 또는 비밀번호를 입력해주세요
           </VaildMessage>
         )}
-        <IdInput onChange={(e) => handleIdInput(e)} value={inputIdValue} />
-        <PasswordInput
-          onChange={(e) => handlePwInput(e)}
-          value={inputPwValue}
-        />
+
+        <IdInput onChange={(e) => handleIdInput(e)} />
+        <PasswordInput onChange={(e) => handlePwInput(e)} />
         <LoginButton onClick={checkLogin}>로그인</LoginButton>
         <Bar />
         <SignButton>회원가입</SignButton>
