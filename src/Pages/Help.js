76c1e6ab529986e9b779dfaf9079c6991c5537db
yisu-kim <yisu.kim.dev@outlook.com>
2021-08-03 16:06:42 +0900

    Refactor: Add index.js file of Modal component

--- src/Pages/Help.js
+++ src/Pages/Help.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
-import Modal from 'Modal/Modal';
+import Modal from 'Modal';
 import ModalChildExample from 'Modal/ModalChildExample';
 
 // Modal Sample
