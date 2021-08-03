720eb54ad18966886a0a799729d21a6ed7c79f7f
kwak-bs <iam-bs@naver.com>
2021-08-04 01:01:56 +0900

    Style : Delete annotation

--- src/Modal/Modal.js
+++ src/Modal/Modal.js
@@ -1,39 +1,39 @@
-// import React from 'react';
-// import ReactDom from 'react-dom';
-// import PropTypes from 'prop-types';
-// import { style } from './ModalStyle';
-// import { AiOutlineClose } from 'react-icons/ai';
+import React from 'react';
+import ReactDom from 'react-dom';
+import PropTypes from 'prop-types';
+import { style } from './ModalStyle';
+import { AiOutlineClose } from 'react-icons/ai';
 
-// export default function Modal({ show, onClose, children }) {
-//   return ReactDom.createPortal(
-//     <>
-//       {show && (
-//         <>
-//           <Overlay />
-//           <Container>
-//             <Wrap>
-//               <Header>
-//                 <ModalClose onClick={onClose}>
-//                   <AiOutlineClose size={18} />
-//                 </ModalClose>
-//               </Header>
-//               <Contents>{children}</Contents>
-//             </Wrap>
-//           </Container>
-//         </>
-//       )}
-//     </>,
-//     document.getElementById('modal-root'),
-//   );
-// }
+export default function Modal({ show, onClose, children }) {
+  return ReactDom.createPortal(
+    <>
+      {show && (
+        <>
+          <Overlay />
+          <Container>
+            <Wrap>
+              <Header>
+                <ModalClose onClick={onClose}>
+                  <AiOutlineClose size={18} />
+                </ModalClose>
+              </Header>
+              <Contents>{children}</Contents>
+            </Wrap>
+          </Container>
+        </>
+      )}
+    </>,
+    document.getElementById('modal-root'),
+  );
+}
 
-// Modal.propTypes = {
-//   show: PropTypes.bool,
-//   onClose: PropTypes.func,
-//   children: PropTypes.oneOfType([
-//     PropTypes.arrayOf(PropTypes.node),
-//     PropTypes.node,
-//   ]),
-// };
+Modal.propTypes = {
+  show: PropTypes.bool,
+  onClose: PropTypes.func,
+  children: PropTypes.oneOfType([
+    PropTypes.arrayOf(PropTypes.node),
+    PropTypes.node,
+  ]),
+};
 
-// const { Overlay, Container, Wrap, Header, ModalClose, Contents } = style;
+const { Overlay, Container, Wrap, Header, ModalClose, Contents } = style;
