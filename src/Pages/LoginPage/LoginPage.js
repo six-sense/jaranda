5483de17acad49866b20ae1442e990549ca35857
UlongChaS2 <jyujung2@gmail.com>
2021-08-03 20:56:35 +0900

    Chore: modifing Login valid Feat: modify and done loginValid Feat: change vaild condition nub

--- src/Pages/LoginPage/LoginPage.js
+++ src/Pages/LoginPage/LoginPage.js
@@ -1,8 +1,27 @@
-import React from 'react'; // , { useState }
+import React, { useState } from 'react';
+import { Validation } from 'utils/checkValid';
 import { style } from './LoginPageStyle';
 
 export default function Login() {
+  const { checkId, checkPassword } = Validation;
   // const [isVaild, setIsVaild] = useState(false);
+  const [inputIdValue, setInputIdValue] = useState('');
+  const [inputPwValue, setInputPwValue] = useState('');
+
+  const handleIdInput = (e) => {
+    console.log(e);
+    const { value } = e.target;
+    if (checkId(value)) {
+      setInputIdValue(value);
+    }
+  };
+
+  const handlePwInput = (e) => {
+    const { value } = e.target;
+    if (checkPassword(value)) {
+      setInputPwValue(value);
+    }
+  };
 
   return (
     <Container>
@@ -13,9 +32,11 @@ export default function Login() {
             유효한 아이디 또는 비밀번호를 입력해주세요
           </VaildMessage>
         )} */}
-
-        <IdInput />
-        <PasswordInput />
+        <IdInput onChange={(e) => handleIdInput(e)} value={inputIdValue} />
+        <PasswordInput
+          onChange={(e) => handlePwInput(e)}
+          value={inputPwValue}
+        />
         <LoginButton>로그인</LoginButton>
         <Bar />
         <SignButton>회원가입</SignButton>
---
+++ src/utils/checkValid.js
@@ -0,0 +1,13 @@
+// 이메일 유효성 검사
+function checkId(id) {
+  const idValid = /^[a-zA-Z0-9_-]{0,26}$/;
+  return id.match(idValid);
+}
+
+//비밀번호 유효성 검사
+function checkPassword(password) {
+  const pwValid = /^[a-zA-Z0-9_-]{0,16}$/;
+  return password.match(pwValid);
+}
+
+export const Validation = { checkPassword, checkId };
