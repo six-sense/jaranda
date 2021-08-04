90fac0b5e7438c143ce2b7f23d1b0a999bf92f3a
Telling-Y <mint1480123@gmail.com>
2021-08-04 16:43:04 +0900

    fix: fix login valid err

--- src/utils/checkValid.js
+++ src/utils/checkValid.js
@@ -1,20 +1,3 @@
-<<<<<<< HEAD
-// 이메일 유효성 검사
-function checkId(e) {
-  const idValid =
-    /^[0-9a-zA-Z]([-_]?[0-9a-zA-Z])*[0-9a-zA-Z]([-_]?[0-9a-zA-Z])*[a-zA-Z]{4,26}$/;
-  return idValid.test(e);
-}
-
-//비밀번호 유효성 검사
-function checkPassword(e) {
-  const pwValid =
-    /^(?=.*[A-Za-z])(?=.*\d)(?=.*[$@$!%*#?&])[A-Za-z\d$@$!%*#?&]{8,13}$/;
-  return pwValid.test(e);
-}
-
-export const Validation = { checkPassword, checkId };
-=======
 // 아이디 유효성 검사
 function checkId(id) {
   const idValid = /^[a-zA-Z0-9_-]{0,26}$/;
@@ -35,4 +18,3 @@ function checkEmail(email) {
 }
 
 export const Validation = { checkPassword, checkId, checkEmail };
->>>>>>> a9f624d7a8b9a5c9f722a5815b309b00cd66e83f
