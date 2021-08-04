101f153c71b98fd770b1cf0b1b91a2f3345afc5c
yh1120 <yonghun1120@gmail.com>
2021-08-04 15:03:38 +0900

    Feat: #73 adminPage Table load with searching, pagination function

--- src/Pages/AdminPage/AdminPage.js
+++ src/Pages/AdminPage/AdminPage.js
@@ -2,9 +2,9 @@ import React, { useEffect, useState } from 'react';
 import searchIcon from 'Assets/search.png';
 import { style } from './AdminPageStyle';
 import { AiOutlineRight, AiOutlineLeft } from 'react-icons/ai';
-import userData from 'utils/userData.json';
 import { useHistory } from 'react-router-dom';
 import { ROUTES } from 'utils/constants';
+import { getUserInfo } from 'utils/getUserInfo';
 
 const properties = [
   { label: 'Parents', value: 'Parents' },
@@ -13,9 +13,12 @@ const properties = [
 
 function AdminPage() {
   const history = useHistory();
-  const [datas, setDatas] = useState([]);
-  const [, setSearchValue] = useState('');
+  const [data, setData] = useState([]);
+  const [searchValue, setSearchValue] = useState('');
   const [checkedArray, setCheckedArray] = useState([]);
+  const [pages, setPages] = useState(1);
+  const [maxPage, setMaxPage] = useState(1);
+  const limit = 10;
 
   const onHandleSearch = (e) => {
     setSearchValue(e.target.value);
@@ -31,13 +34,35 @@ function AdminPage() {
     setCheckedArray(newChecked);
   };
 
+  const onHandleButtonLeft = () => {
+    const page = pages - 1;
+    if (page < 1) {
+      setPages(1);
+    } else {
+      setPages(page);
+    }
+  };
+
+  const onHandleButtonRight = () => {
+    const page = pages + 1;
+    if (page > maxPage) {
+      setPages(maxPage);
+    } else {
+      setPages(page);
+    }
+  };
+
   const goRoleManagementPage = () => {
     history.push(ROUTES.ROLE_MANAGEMENT);
   };
 
   useEffect(() => {
-    setDatas(userData);
-  }, []);
+    console.log(pages);
+    const userInfo = getUserInfo(pages, limit, searchValue);
+    console.log('userInfo', userInfo);
+    setData(userInfo.userData);
+    setMaxPage(userInfo.maxPage);
+  }, [pages, searchValue]);
 
   return (
     <div>
@@ -66,31 +91,32 @@ function AdminPage() {
             </tr>
           </thead>
           <tbody>
-            {datas.map((data, index) => (
-              <tr key={index}>
-                <Cell>{data.name}</Cell>
-                <Cell>{data.age}</Cell>
-                <Cell>{data.address}</Cell>
-                <Cell>
-                  {properties.map((property, index) => (
-                    <div key={index}>
-                      <CheckButton
-                        type="checkbox"
-                        id={index}
-                        onChange={() => onHandleButton(property.value)}
-                      />
-                      <span>{property.label}</span>
-                    </div>
-                  ))}
-                </Cell>
-              </tr>
-            ))}
+            {data &&
+              data.map((data, index) => (
+                <tr key={index}>
+                  <Cell>{data.name}</Cell>
+                  <Cell>{data.age}</Cell>
+                  <Cell>{data.address}</Cell>
+                  <Cell>
+                    {properties.map((property, index) => (
+                      <div key={index}>
+                        <CheckButton
+                          type="checkbox"
+                          id={index}
+                          onChange={() => onHandleButton(property.value)}
+                        />
+                        <span>{property.label}</span>
+                      </div>
+                    ))}
+                  </Cell>
+                </tr>
+              ))}
           </tbody>
         </table>
         <TableFooter>
           <div>
-            <AiOutlineLeft />
-            <AiOutlineRight />
+            <AiOutlineLeft onClick={onHandleButtonLeft} />
+            <AiOutlineRight onClick={onHandleButtonRight} />
           </div>
         </TableFooter>
       </TableContainer>
