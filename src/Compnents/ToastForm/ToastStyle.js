17e83c673e7b6e8b8c83212cd048552e3424493b
Yisu Kim <yisu.kim.dev@outlook.com>
2021-08-04 14:02:47 +0900

    Feat: #75 Add toast message for CreditCardForm

--- src/Compnents/ToastForm/ToastStyle.js
+++ src/Compnents/ToastForm/ToastStyle.js
@@ -1,47 +1,37 @@
 import styled from 'styled-components';
-export const Overlay = styled.div`
-        position: fixed;
-        top: 0;
-        left: 0;
-        right: 0;
-        bottom: 0;
-        background-color: rgba(0, 0, 0, 0.2);
-        z-index: 1;
-    `;
+
 export const Container = styled.div`
   position: fixed;
   top: 20%;
   left: 50%;
   background-color: #00000060;
-  border-radius:20px;
+  border-radius: 20px;
   transform: translate(-50%, -50%);
   z-index: 1;
 `;
 export const Wrap = styled.div`
   ${({ theme }) => theme.flexSet('center', 'center', 'column')};
   width: auto;
-  margin-left:10px;
-  margin-right:10px;
+  margin-left: 10px;
+  margin-right: 10px;
 `;
 export const Header = styled.div`
   position: relative;
   width: 100%;
-  height:1.5rem;
-  font-size:13px;
-  display:flex;
-  align-items:center;
-  
+  height: 1.5rem;
+  font-size: 13px;
+  display: flex;
+  align-items: center;
 `;
 export const Contents = styled.div`
-    width:100%;
-    text-align:center;
-    font-size:13px;
-    color:#FFFFFF;
+  width: 100%;
+  text-align: center;
+  font-size: 13px;
+  color: #ffffff;
 `;
 export const style = {
-    Overlay,
-    Container,
-    Wrap,
-    Header,
-    Contents,
-  };
+  Container,
+  Wrap,
+  Header,
+  Contents,
+};
