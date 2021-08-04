545279f222b80e0a9fc6fc052c621fef2f5415b9
kwak-bs <iam-bs@naver.com>
2021-08-05 00:28:46 +0900

    Feat : Add checkIdSignUp 회원 가입용 아이디 유효성 검사 정규식 추가

--- src/utils/checkValid.js
+++ src/utils/checkValid.js
@@ -12,9 +12,14 @@ function checkPassword(password) {
 
 // 비밀번호 유효성 검사 (8자리 이상/ 숫자,알파벳,특수문자 필수 포함)
 function checkPasswordSignUp(password) {
-  const pwValid =
-  /^(?=.*[a-zA-Z])(?=.*[!@#$%^*+=-])(?=.*[0-9]).{8,25}$/;
+  const pwValid = /^(?=.*[a-zA-Z])(?=.*[!@#$%^*+=-])(?=.*[0-9]).{8,25}$/;
   return password.match(pwValid);
+}
+
+// 아이디 유효성 검사 (5자리 이상)
+function checkIdSignUp(id) {
+  const idValid = /^[a-zA-Z0-9_-]{5,26}$/;
+  return id.match(idValid);
 }
 
 // 이메일 유효성 검사
@@ -24,4 +29,10 @@ function checkEmail(email) {
   return email.match(emailValid);
 }
 
-export const Validation = { checkPassword, checkId, checkEmail, checkPasswordSignUp };
+export const Validation = {
+  checkIdSignUp,
+  checkPassword,
+  checkId,
+  checkEmail,
+  checkPasswordSignUp,
+};
